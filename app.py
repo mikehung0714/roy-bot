ab8405dbac5396b7f629228ab86679d1a449ed0c
mikehung0714 <mikehung0714@gmail.com>
2020-02-18 15:49:32 +0800

    老師?!?!?

--- app.py
+++ app.py
@@ -32,7 +32,23 @@ def callback():
 #之後所有機器人判斷邏輯的編輯區
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='我聽不懂')
+    message = TemplateSendMessage(
+        alt_text='請傳送座標',
+        template=ConfirmTemplate(
+            text='查詢附近的特約藥局，是否傳送您的經位度座標？',
+            actions=[
+                URIAction(
+                    label='前往地圖',
+                    uri='line://nv/location'
+                ),
+                MessageAction(
+                    label='取消',
+                    text='下次再查'
+                )
+            ]
+        )
+    )
+
     line_bot_api.reply_message(event.reply_token, message)
 
 # 處理地點訊息，並且回傳經緯度資料
